a52ca05298f03dabdb6bf60ceaae81d6fc2e2ba4
mszpak <moszee@gmail.com>
2016-12-16 21:38:31 +0100

    Initial commit

---
+++ src/main/java/org/szpax/brothers/GymBackendApplication.java
@@ -0,0 +1,12 @@
+package org.szpax.brothers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBackendApplication.class, args);
+	}
+}
